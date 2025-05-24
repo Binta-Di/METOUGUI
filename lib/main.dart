6f63601154bac715c8addd274d49549321598989
ibrahimaassiatoudiallo <ibrahimaassiatou.diallo2@orange-sonatel.com>
2025-05-24 17:51:20 +0000

    fix key

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:get/get.dart';
 import 'package:travee/screens/homescreen/bottom_nav_bar.dart';
 
 void main() {
-  String gptToken = "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";
-  String gptApiKey = "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";
+  // String gptToken = "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";
+  // String gptApiKey = "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";
   // String orgId = "org-3EMmsvASfykyFOG55GAadAch";
 
-  OpenAI.apiKey = gptApiKey;
+  // OpenAI.apiKey = gptApiKey;
   // OpenAI.organization = orgId;
   runApp(const MyApp()); // travee app
   //runApp(const DemoApp()); // animation class
